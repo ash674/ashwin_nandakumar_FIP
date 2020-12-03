95ccf500e4282b5630f355018c96cf3a0e051e4f
ashwin <ashu61998@gmail.com>
2020-12-04 01:28:07 +0530

    css updated js added

--- js/main.js
+++ js/main.js
@@ -1,22 +1,54 @@
-import aboutData from '../data/data.js';
+import { fetchData } from "./modules/TheDataMiner.js";
+import MiniCard from "./modules/AppCard.js";
+import moreContent from "./modules/moreContent.js";
 
-(() =>
- {
-  fetch ('./data/data.json')
-  .then(res => res.json())
+(() => {
 
-  .then(data => {console.log(data)
-handleDataSet(data)})
-function handleDataSet(data){
-    
-       for(let user in data){
-        let  aboutText = document.querySelector(".aboutText"),  
-        aboutTextInner = aboutText.children;
-        
-        aboutTextInner[0].textContent = aboutData.name 
-        aboutTextInner[1].textContent = aboutData.job;
-          aboutTextInner[2].textContent = aboutData.about;
-       }
-   }
-   
+    let vue_vm = new Vue({
+        data: {  
+          message: "Projects",
+        anotherMessage: "Click on image to see description",
+        removeAformat: true,
+        showBioData: false,
+        miniCars: [],
+        currentMiniData: {}
+        },
+
+      
+        mounted: function() {
+            console.log("Vue is mounted, trying a fetch for the initial data");
+            
+            fetchData("./includes/index.php").then(data => this.miniCars = data).catch(err => JSON.parse(JSON.stringify(err)));
+              
+        },
+
+        // run a method when we change our view (update the DOM with Vue)
+        updated: function() {
+            console.log('Vue just updated the DOM');
+        },
+
+        methods: {
+            logClicked() {
+                console.log("clicked on a list item");
+            },
+
+            clickHeader() {
+                console.log("clicked on the header");
+            },
+
+            showMiniData(target) {
+                
+             console.log('clicked to view', target.model);  
+                  this.showBioData = this.showBioData ? false : true;
+
+                 this.currentMiniData = target;
+            }            
+
+        },
+
+        components: {
+            "mini-card": MiniCard,
+         "content-card": moreContent
+        }
+    }).$mount("#app1"); // also connects Vue to your wrapper in HTML
 })();
